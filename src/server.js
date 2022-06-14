25e1fa83761dfb60195711fe5def5e2190c45ca0
User <user@Users-MBP.lan>
2022-06-14 16:27:53 +0100

    working with browser

--- src/server.js
+++ src/server.js
@@ -1,1 +1,8 @@
 const express = require("express"); //Lets us use express.js
+const app = express(); // Creates webserver and stores in app
+
+app.use("/static", express.static("public")); // Targets our public folder
+
+app.listen(5001, () => {
+    console.log("Listening on port 5001")
+} )
