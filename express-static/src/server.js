f75cbe1fd06efe70a0fabb41f270639a5e42e3b8
Luka <lukafrance@gmail.com>
2022-06-14 17:16:28 +0100

    more href

---
+++ express-static/src/server.js
@@ -0,0 +1,10 @@
+const express = require("express"); //Lets us use express.js
+const app = express(); // Creates webserver and stores in app
+
+app.use("/static", express.static("public")); // Targets our public folder
+
+app.listen(5001, () => {
+    console.log("Listening on port 5001")
+} )
+
+//Access wesbite: http://localhost:5001/static/
